a13d1f7e0dd63642527cc3b51a34a86e588b93ea
Antoine Duchenet <antoine.d@zip.fr>
2016-03-29 15:47:45 +0200

    iOS register.

--- www/js/app.js
+++ www/js/app.js
@@ -5,131 +5,170 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', 
-  ['ionic',
-   'starter.controllers',
-   'starter.services',
-   'ngCordova'
-  ])
+angular.module('starter',
+['ionic',
+'starter.controllers',
+'starter.services',
+'ngCordova'
+])
 
 .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+    $ionicPlatform.ready(function() {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
+        }
+        if (window.StatusBar) {
+            // org.apache.cordova.statusbar required
+            StatusBar.styleDefault();
+        }
+    });
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
+    // Ionic uses AngularUI Router which uses the concept of states
+    // Learn more here: https://github.com/angular-ui/ui-router
+    // Set up the various states which the app can be in.
+    // Each state's controller can be found in controllers.js
+    $stateProvider
 
-  // setup an abstract state for the tabs directive
+    // setup an abstract state for the tabs directive
     .state('tab', {
-    url: '/tab',
-    abstract: true,
-    templateUrl: 'templates/tabs.html'
-  })
+        url: '/tab',
+        abstract: true,
+        templateUrl: 'templates/tabs.html'
+    })
 
-  // Each tab has its own nav history stack:
+    // Each tab has its own nav history stack:
 
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
-  })
-
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
+    .state('tab.dash', {
+        url: '/dash',
+        views: {
+            'tab-dash': {
+                templateUrl: 'templates/tab-dash.html',
+                controller: 'DashCtrl'
+            }
         }
-      }
+    })
+
+    .state('tab.chats', {
+        url: '/chats',
+        views: {
+            'tab-chats': {
+                templateUrl: 'templates/tab-chats.html',
+                controller: 'ChatsCtrl'
+            }
+        }
     })
     .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
+        url: '/chats/:chatId',
+        views: {
+            'tab-chats': {
+                templateUrl: 'templates/chat-detail.html',
+                controller: 'ChatDetailCtrl'
+            }
         }
-      }
     })
 
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
-  });
+    .state('tab.account', {
+        url: '/account',
+        views: {
+            'tab-account': {
+                templateUrl: 'templates/tab-account.html',
+                controller: 'AccountCtrl'
+            }
+        }
+    });
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/tab/dash');
 
 })
 .run(function($cordovaPush,$ionicPlatform,$rootScope) {
 
-  var androidConfig = {
-    "senderID": "775034540259",
-  };
+    var androidConfig = {
+        "senderID": "775034540259",
+    };
+
+    var iosConfig = {
+        "badge": true,
+        "sound": true,
+        "alert": true,
+    };
 
 
-  $ionicPlatform.ready(function(){
+    ionic.Platform.ready(function(){
+        if(ionic.Platform.platform() === 'ios') {
+            $cordovaPush.register(iosConfig).then(function(deviceToken) {
+                console.log("deviceToken: " + deviceToken)
+            }, function(err) {
+                alert("Registration error: " + err)
+            });
 
-    $cordovaPush.register(androidConfig).then(function(result) {
-      // Success
-       console.log('test');
-    }, function(err) {
-      // Error
-       console.log(err);
-    })
+            $rootScope.$on('$cordovaPush:notificationReceived', function(event, notification) {
+                if (notification.alert) {
+                    navigator.notification.alert(notification.alert);
+                }
 
-    $rootScope.$on('$cordovaPush:notificationReceived', function(event, notification) {
-      switch(notification.event) {
-        case 'registered':
-          if (notification.regid.length > 0 ) {
-            console.log('registration ID = ' + notification.regid);
-          }
-          break;
+                if (notification.sound) {
+                    var snd = new Media(event.sound);
+                    snd.play();
+                }
 
-        case 'message':
-          console.log(notification);
-          // this is the actual push notification. its format depends on the data model from the push server
-          alert(notification.payload['gcm.notification.body']);
-          break;
+                if (notification.badge) {
+                    $cordovaPush.setBadgeNumber(notification.badge).then(function(result) {
+                        // Success!
+                    }, function(err) {
+                        // An error occurred. Show a message to the user
+                    });
+                }
+            });
+        } else if(ionic.Platform.platform() === 'android') {
+            $cordovaPush.register(iosConfig).then(function(deviceToken) {
+                console.log("deviceToken: " + deviceToken)
+            }, function(err) {
+                alert("Registration error: " + err)
+            });
 
-        case 'error':
-          alert('GCM error = ' + notification.msg);
-          break;
+            $cordovaPush.register(androidConfig).then(function(result) {
+                // Success
+                console.log('register success');
+            }, function(err) {
+                // Error
+                console.log(err);
+            })
 
-        default:
-          alert('An unknown GCM event has occurred');
-          break;
-      }
+            $rootScope.$on('$cordovaPush:notificationReceived', function(event, notification) {
+                console.log(notification);
+                switch(notification.event) {
+                    case 'registered':
+                    if (notification.regid.length > 0 ) {
+                        console.log('registration ID = ' + notification.regid);
+                    }
+                    break;
+
+                    case 'message':
+                    console.log(notification);
+                    // this is the actual push notification. its format depends on the data model from the push server
+                    alert(notification.payload['gcm.notification.body']);
+                    break;
+
+                    case 'error':
+                    alert('GCM error = ' + notification.msg);
+                    break;
+
+                    default:
+                    alert('An unknown GCM event has occurred');
+                    break;
+                }
+            });
+        }
     });
 
-
-  });
+});
 
 });
